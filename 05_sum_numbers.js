fa4126ba77b23144bd11fdad1a82da85923f8c99
caadmin <janel.brandon@coderacademy.edu.au>
2018-11-16 11:18:09 +1000

    error in tests

--- 05_sum_numbers.js
+++ 05_sum_numbers.js
@@ -21,15 +21,15 @@ class Player {
 let players = []
 describe('findWinner', function () {
     it('Should return the winner when winner is first in array', function () {
-        players = [new Player('James', [50, 100, 85], new Player('Nathan', [55, 90, 86]))]
+        players = [new Player('James', [50, 100, 85]), new Player('Nathan', [55, 90, 86])]
         assert.equal('James', findWinner(players))
     })
     it('Should return the winner when winner is second in array', function () {
-        players = [new Player('Nathan', [50, 100, 85], new Player('James', [55, 90, 86]))]
+        players = [new Player('Nathan', [50, 100, 85]), new Player('James', [55, 90, 86])]
         assert.equal('Nathan', findWinner(players))
     })
     it('Should return the first player when both players are tied', function () {
-        players = [new Player('Nathan', [50, 100, 85], new Player('James', [50, 100, 85]))]
+        players = [new Player('Nathan', [50, 100, 85]), new Player('James', [50, 100, 85])]
         assert.equal('Nathan', findWinner(players))
     })
 })
